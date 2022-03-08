562cf2ac7e4caaaf48f8154942e50cedf1df72bd
Collin Brittain <collinbrittain@gmail.com>
2022-03-08 16:23:35 -0600

    Integration test with chains enum

--- ocular/tests/rpc.rs
+++ ocular/tests/rpc.rs
@@ -1,9 +1,16 @@
 use assay::assay;
-use ocular::chain::client::ChainClient;
+use ocular::chain::{client::ChainClient, Chains};
 
 #[assay]
 async fn create_chain_client() {
-    let client = ChainClient::new("cosmoshub");
+    let client = ChainClient::new(Chains::CosmosHub);
+
+    client.unwrap();
+}
+
+#[assay]
+async fn create_unsupported_chain_client() {
+    let client = ChainClient::new_unsupported("cosmoshub");
 
     client.unwrap();
 }
