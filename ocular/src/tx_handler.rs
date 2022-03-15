5cc0216d0c00bdcc95e481a4c95536981390fff1
Phil <pbal@fordham.edu>
2022-03-15 15:16:09 -0400

    add test

--- ocular/src/tx_handler.rs
+++ ocular/src/tx_handler.rs
@@ -10,6 +10,16 @@ use cosmrs::{
 /// Transaction Handler off of which all transactions can be performed from.
 pub struct TransactionHandler {}
 
+/// Metadata for transaction
+pub struct TransactionMetadata {
+    pub chain_id: Id,
+    pub account_number: u64,
+    pub sequence_number: u64,
+    pub gas_limit: u64,
+    pub timeout_height: u16,
+    pub memo: String,
+}
+
 impl TransactionHandler {
     /// Creates and signs a send message. Returns an error if unsuccessful in creating a Tx.
     pub fn create_and_sign_send_message(
@@ -18,34 +28,37 @@ impl TransactionHandler {
         sender_private_key: SigningKey,
         recipient_account: AccountId,
         amount: Coin,
-        chain_id: &Id,
-        account_number: u64,
-        sequence_number: u64,
-        gas_limit: u64,
-        timeout_height: u16,
-        memo: &str,
+        tx_metadata: TransactionMetadata,
     ) -> Result<Tx, TransactionError> {
         // Create send message for coin
         let msg_send = MsgSend {
-            from_address: sender_account.clone(),
+            from_address: sender_account,
             to_address: recipient_account,
             amount: vec![amount.clone()],
         };
 
         // Build tx body.
         let tx_body = match msg_send.to_any() {
-            Ok(msg) => tx::Body::new(vec![msg], memo, timeout_height),
+            Ok(msg) => tx::Body::new(vec![msg], tx_metadata.memo, tx_metadata.timeout_height),
             Err(err) => return Err(TransactionError::SerializationError(err.to_string())),
         };
 
         // Create signer info.
-        let signer_info = SignerInfo::single_direct(Some(sender_public_key), sequence_number);
+        let signer_info =
+            SignerInfo::single_direct(Some(sender_public_key), tx_metadata.sequence_number);
 
         // Compute auth info from signer info by associating a fee.
-        let auth_info = signer_info.auth_info(Fee::from_amount_and_gas(amount, gas_limit));
+        // TODO: Add ability to specify payers and granters for fees.
+        let auth_info =
+            signer_info.auth_info(Fee::from_amount_and_gas(amount, tx_metadata.gas_limit));
 
         // Create doc to be signed
-        let sign_doc = match SignDoc::new(&tx_body, &auth_info, chain_id, account_number) {
+        let sign_doc = match SignDoc::new(
+            &tx_body,
+            &auth_info,
+            &tx_metadata.chain_id,
+            tx_metadata.account_number,
+        ) {
             Ok(doc) => doc,
             Err(err) => return Err(TransactionError::TypeConversionError(err.to_string())),
         };
@@ -65,7 +78,7 @@ impl TransactionHandler {
         // Convert to cosmrs Tx
         match Tx::from_bytes(&tx_bytes) {
             Ok(tx) => Ok(tx),
-            Err(err) => return Err(TransactionError::TypeConversionError(err.to_string())),
+            Err(err) => Err(TransactionError::TypeConversionError(err.to_string())),
         }
     }
 }
@@ -76,7 +89,40 @@ mod tests {
     use super::*;
 
     #[test]
-    fn placeholder() {
-        assert_eq!(true, false);
+    fn create_and_sign_send_message() {
+        // Create some keys to start off
+        let sender_private_key = SigningKey::random();
+        let sender_public_key = sender_private_key.public_key();
+        let sender_account = sender_public_key
+            .account_id("cosmos")
+            .expect("Could not create sender account.");
+
+        let recipient_account = SigningKey::random()
+            .public_key()
+            .account_id("cosmos")
+            .expect("Could not create recipient account");
+
+        let tx = TransactionHandler::create_and_sign_send_message(
+            sender_account,
+            sender_public_key,
+            sender_private_key,
+            recipient_account,
+            Coin {
+                amount: 1_000_000u64.into(),
+                denom: "uatom".parse().unwrap(),
+            },
+            TransactionMetadata {
+                chain_id: Id::try_from("cosmoshub-4").expect("Could not create chain id."),
+                account_number: 1,
+                sequence_number: 0,
+                gas_limit: 100_000,
+                timeout_height: 9001u16,
+                memo: String::from("Some memo"),
+            },
+        )
+        .expect("Could not create tx");
+
+        dbg!(tx.body);
+        dbg!(tx.auth_info);
     }
 }
