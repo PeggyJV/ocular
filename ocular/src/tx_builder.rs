a4b4586153c662eaf50f965d1ce28d17da77d483
Phil <pbal@fordham.edu>
2022-03-17 13:41:11 -0400

    Add delegate and undelegate msg builders

--- ocular/src/tx_builder.rs
+++ ocular/src/tx_builder.rs
@@ -2,6 +2,7 @@ use crate::error::TransactionError;
 use cosmrs::{
     bank::MsgSend,
     crypto::{secp256k1::SigningKey, PublicKey},
+    staking::{MsgDelegate, MsgUndelegate},
     tendermint::chain::Id,
     tx::{self, Fee, Msg, SignDoc, SignerInfo},
     AccountId, Coin,
@@ -75,15 +76,15 @@ impl TransactionHandler {
         amount: Coin,
         tx_metadata: TransactionMetadata,
     ) -> Result<Vec<u8>, TransactionError> {
-        // Create send message for coin
-        let msg_send = MsgSend {
+        // Create send message for amount
+        let msg = MsgSend {
             from_address: sender_account,
             to_address: recipient_account,
             amount: vec![amount.clone()],
         };
 
         // Build tx body.
-        let tx_body = match msg_send.to_any() {
+        let tx_body = match msg.to_any() {
             Ok(msg) => tx::Body::new(vec![msg], &tx_metadata.memo, tx_metadata.timeout_height),
             Err(err) => return Err(TransactionError::SerializationError(err.to_string())),
         };
@@ -91,6 +92,70 @@ impl TransactionHandler {
         self.sign_and_serialize_msg_into_bytes(
             sender_public_key,
             sender_private_key,
+            amount,
+            tx_body,
+            tx_metadata,
+        )
+    }
+
+    /// Sign and create delegate message
+    pub fn create_and_sign_msg_delegate(
+        &self,
+        delegator_account: AccountId,
+        delegator_public_key: PublicKey,
+        delegator_private_key: SigningKey,
+        validator_account: AccountId,
+        amount: Coin,
+        tx_metadata: TransactionMetadata,
+    ) -> Result<Vec<u8>, TransactionError> {
+        // Create delegate message for amount
+        let msg = MsgDelegate {
+            delegator_address: delegator_account,
+            validator_address: validator_account,
+            amount: amount.clone(),
+        };
+
+        // Build tx body.
+        let tx_body = match msg.to_any() {
+            Ok(msg) => tx::Body::new(vec![msg], &tx_metadata.memo, tx_metadata.timeout_height),
+            Err(err) => return Err(TransactionError::SerializationError(err.to_string())),
+        };
+
+        self.sign_and_serialize_msg_into_bytes(
+            delegator_public_key,
+            delegator_private_key,
+            amount,
+            tx_body,
+            tx_metadata,
+        )
+    }
+
+    /// Sign and create undelegate message
+    pub fn create_and_sign_msg_undelegate(
+        &self,
+        delegator_account: AccountId,
+        delegator_public_key: PublicKey,
+        delegator_private_key: SigningKey,
+        validator_account: AccountId,
+        amount: Coin,
+        tx_metadata: TransactionMetadata,
+    ) -> Result<Vec<u8>, TransactionError> {
+        // Create undelegate message for amount
+        let msg = MsgUndelegate {
+            delegator_address: delegator_account,
+            validator_address: validator_account,
+            amount: amount.clone(),
+        };
+
+        // Build tx body.
+        let tx_body = match msg.to_any() {
+            Ok(msg) => tx::Body::new(vec![msg], &tx_metadata.memo, tx_metadata.timeout_height),
+            Err(err) => return Err(TransactionError::SerializationError(err.to_string())),
+        };
+
+        self.sign_and_serialize_msg_into_bytes(
+            delegator_public_key,
+            delegator_private_key,
             amount,
             tx_body,
             tx_metadata,
@@ -121,6 +186,82 @@ mod tests {
 
         transaction_handler
             .create_and_sign_msg_send(
+                sender_account,
+                sender_public_key,
+                sender_private_key,
+                recipient_account,
+                Coin {
+                    amount: 1_000_000u64.into(),
+                    denom: "uatom".parse().unwrap(),
+                },
+                TransactionMetadata {
+                    chain_id: Id::try_from("cosmoshub-4").expect("Could not create chain id."),
+                    account_number: 1,
+                    sequence_number: 0,
+                    gas_limit: 100_000,
+                    timeout_height: 9001u16,
+                    memo: String::from("Some memo"),
+                },
+            )
+            .expect("Could not create tx");
+    }
+
+    #[test]
+    fn create_and_sign_msg_delegate() {
+        // Create some keys to start off
+        let sender_private_key = SigningKey::random();
+        let sender_public_key = sender_private_key.public_key();
+        let sender_account = sender_public_key
+            .account_id("cosmos")
+            .expect("Could not create sender account.");
+
+        let recipient_account = SigningKey::random()
+            .public_key()
+            .account_id("cosmos")
+            .expect("Could not create recipient account");
+
+        let transaction_handler = TransactionHandler {};
+
+        transaction_handler
+            .create_and_sign_msg_delegate(
+                sender_account,
+                sender_public_key,
+                sender_private_key,
+                recipient_account,
+                Coin {
+                    amount: 1_000_000u64.into(),
+                    denom: "uatom".parse().unwrap(),
+                },
+                TransactionMetadata {
+                    chain_id: Id::try_from("cosmoshub-4").expect("Could not create chain id."),
+                    account_number: 1,
+                    sequence_number: 0,
+                    gas_limit: 100_000,
+                    timeout_height: 9001u16,
+                    memo: String::from("Some memo"),
+                },
+            )
+            .expect("Could not create tx");
+    }
+
+    #[test]
+    fn create_and_sign_msg_undelegate() {
+        // Create some keys to start off
+        let sender_private_key = SigningKey::random();
+        let sender_public_key = sender_private_key.public_key();
+        let sender_account = sender_public_key
+            .account_id("cosmos")
+            .expect("Could not create sender account.");
+
+        let recipient_account = SigningKey::random()
+            .public_key()
+            .account_id("cosmos")
+            .expect("Could not create recipient account");
+
+        let transaction_handler = TransactionHandler {};
+
+        transaction_handler
+            .create_and_sign_msg_undelegate(
                 sender_account,
                 sender_public_key,
                 sender_private_key,
