829f0a1d17c08b5e75a7a09dbdfb9e3f4ccf563e
Phil <pbal@fordham.edu>
2022-05-17 14:15:14 -0400

    Clean up removed crate import

--- ocular_cli/src/commands/chains/list.rs
+++ ocular_cli/src/commands/chains/list.rs
@@ -1,6 +1,5 @@
-use crate::config::LensrsCliConfig;
 use crate::prelude::*;
-use abscissa_core::{config, Command, FrameworkError, Runnable};
+use abscissa_core::{Command, Runnable};
 use clap::Parser;
 
 #[derive(Command, Debug, Parser)]
@@ -25,14 +24,5 @@ impl Runnable for ListCmd {
             status_err!("executor exited with error: {}", e);
             std::process::exit(1);
         });
-    }
-}
-
-impl config::Override<LensrsCliConfig> for ListCmd {
-    // Process the given command line options, overriding settings from
-    // a configuration file using explicit flags taken from command-line
-    // arguments.
-    fn override_config(&self, config: LensrsCliConfig) -> Result<LensrsCliConfig, FrameworkError> {
-        Ok(config)
     }
 }
