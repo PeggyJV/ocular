aacfa7d4de59e418ac21e1df6da22c643e91f2f8
hannydevelop <ukpaiugochiibem@gmail.com>
2022-05-18 13:37:24 +0100

    added functions in config into impl

--- ocular_cli/src/commands/chains/list.rs
+++ ocular_cli/src/commands/chains/list.rs
@@ -1,4 +1,4 @@
-use crate::prelude::*;
+use crate::{prelude::*, application::APP};
 use abscissa_core::{Command, Runnable};
 use clap::Parser;
 
@@ -8,7 +8,14 @@ pub struct ListCmd {}
 impl Runnable for ListCmd {
     /// List all chains
     fn run(&self) {
+        // List chains fro local config
+
+        // step 1:
+        // Run Fs::read on config file
         abscissa_tokio::run(&APP, async {
+            let config = APP.config();
+            let path = config.get_config_path();
+            let config_file = Path::new(path.to_str().unwrap());
             match ocular::chain::registry::list_chains().await {
                 Ok(info) => {
                     let info = serde_json::to_string_pretty(&info).unwrap_or_else(|err| {
--- ocular_cli/src/config.rs
+++ ocular_cli/src/config.rs
@@ -23,51 +23,53 @@ pub struct OcularCliConfig {
     pub chains: Vec<ChainInfo>,
 }
 
-/// Initializes ocular config dir and file if they do not exist.
-pub async fn init() -> Result<PathBuf, Error> {
-    let path = get_config_path();
-    let config_file = Path::new(path.to_str().unwrap());
-    let config_dir = config_file.parent().unwrap();
-    if !config_dir.exists() {
-        println!("config directory does not exist. creating!");
-        if let Err(err) = std::fs::create_dir(config_dir) {
-            return Err(err.into());
+impl OcularCliConfig {
+    /// Builds a config path in the users home directory
+    pub fn get_config_path(&self) -> PathBuf {
+        let mut path = dirs::home_dir().unwrap();
+        path.push(".ocular");
+        path.push("config.toml");
+
+        path
+    }
+
+    /// Initializes ocular config dir and file if they do not exist.
+    pub async fn init(&self) -> Result<PathBuf, Error> {
+        let path = self.get_config_path();
+        let config_file = Path::new(path.to_str().unwrap());
+        let config_dir = config_file.parent().unwrap();
+        if !config_dir.exists() {
+            println!("config directory does not exist. creating!");
+            if let Err(err) = std::fs::create_dir(config_dir) {
+                return Err(err.into());
+            }
         }
+        if !config_file.exists() {
+            debug!("creating config file with default chains");
+            let default_chains = self.get_default_chains().await?;
+            let config_content = OcularCliConfig {
+                default_chain: "cosmoshub".to_string(),
+                chains: default_chains,
+            };
+            let config_content = toml::to_string(&config_content)?;
+            fs::write(config_file, config_content)?
+        }
+
+        Ok(path)
     }
-    if !config_file.exists() {
-        debug!("creating config file with default chains");
-        let default_chains = get_default_chains().await?;
-        let config_content = OcularCliConfig {
-            default_chain: "cosmoshub".to_string(),
-            chains: default_chains,
+
+    async fn get_default_chains(&self) -> Result<Vec<ChainInfo>, Error> {
+        let mut chains = Vec::<ChainInfo>::with_capacity(2);
+
+        match registry::get_chain("cosmoshub").await {
+            Ok(info) => chains.push(info),
+            Err(err) => return Err(err.into()),
         };
-        let config_content = toml::to_string(&config_content)?;
-        fs::write(config_file, config_content)?
+        match registry::get_chain("osmosis").await {
+            Ok(info) => chains.push(info),
+            Err(err) => return Err(err.into()),
+        };
+
+        Ok(chains)
     }
-
-    Ok(path)
-}
-
-/// Builds a config path in the users home directory
-pub fn get_config_path() -> PathBuf {
-    let mut path = dirs::home_dir().unwrap();
-    path.push(".ocular");
-    path.push("config.toml");
-
-    path
-}
-
-async fn get_default_chains() -> Result<Vec<ChainInfo>, Error> {
-    let mut chains = Vec::<ChainInfo>::with_capacity(2);
-
-    match registry::get_chain("cosmoshub").await {
-        Ok(info) => chains.push(info),
-        Err(err) => return Err(err.into()),
-    };
-    match registry::get_chain("osmosis").await {
-        Ok(info) => chains.push(info),
-        Err(err) => return Err(err.into()),
-    };
-
-    Ok(chains)
 }
