adbb8c591b16b799cd49d60455db165d7c82ebdc
hannydevelop <ukpaiugochiibem@gmail.com>
2022-06-01 14:02:09 +0100

    removed ibc from registry list

--- ocular_cli/src/commands/chains/registry_list.rs
+++ ocular_cli/src/commands/chains/registry_list.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
 use abscissa_core::{Command, Runnable};
 use clap::Parser;
+use serde::Serialize;
 
 #[derive(Command, Debug, Parser)]
 pub struct RegistryListCmd {}
@@ -10,12 +11,17 @@ impl Runnable for RegistryListCmd {
     fn run(&self) {
         abscissa_tokio::run(&APP, async {
             match ocular::chain::registry::list_chains().await {
-                Ok(info) => {
-                    let info = serde_json::to_string_pretty(&info).unwrap_or_else(|err| {
-                        status_err!("Can't convert chain info to JSON: {}", err);
-                        std::process::exit(1);
-                    });
-                    print!("{}", info)
+                Ok(mut info) => {
+                    info.drain(0..1);
+                    for info in info {
+                        let info = info.as_str();
+                        let buf = Vec::new();
+                        let formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
+                        let mut serialize = serde_json::Serializer::with_formatter(buf, formatter);
+                        info.serialize(&mut serialize).unwrap();
+
+                        println!("{}", String::from_utf8(serialize.into_inner()).unwrap());
+                    }
                 }
                 Err(err) => error!("{}", err),
             }
