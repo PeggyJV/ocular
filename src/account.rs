c7ffe4069abbb46734121e4bc244c99e92c36089
Collin Brittain <collinbrittain@gmail.com>
2022-08-29 14:55:59 -0500

    Move auth BaseAccount representation to appropriate query module

--- src/account.rs
+++ src/account.rs
@@ -57,32 +57,3 @@ impl From<Arc<SigningKey>> for AccountInfo {
         }
     }
 }
-
-/// Used for converting the BaseAccount type in cosmos_sdk_proto to something with concrete field types
-#[derive(Clone, Debug)]
-pub struct BaseAccount {
-    pub address: String,
-    // public key may not be present on chain
-    pub pub_key: Option<PublicKey>,
-    pub account_number: u64,
-    pub sequence: u64,
-}
-
-// TO-DO: Handle public keys with type URL /cosmos.crypto.multisig.LegacyAminoPubKey
-impl TryFrom<cosmrs::proto::cosmos::auth::v1beta1::BaseAccount> for BaseAccount {
-    type Error = Report;
-
-    fn try_from(account: cosmrs::proto::cosmos::auth::v1beta1::BaseAccount) -> Result<BaseAccount> {
-        let pub_key = match account.pub_key {
-            Some(k) => Some(PublicKey::try_from(k)?),
-            None => None,
-        };
-
-        Ok(BaseAccount {
-            address: account.address,
-            pub_key,
-            account_number: account.account_number,
-            sequence: account.sequence,
-        })
-    }
-}
