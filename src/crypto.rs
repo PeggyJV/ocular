0b58f14fe1e72bc1ab23dc57444c448145f4fce0
Collin Brittain <collinbrittain@gmail.com>
2024-01-19 13:59:27 -0600

    Clippy

--- src/crypto.rs
+++ src/crypto.rs
@@ -4,6 +4,6 @@ use pkcs8::rand_core::OsRng;
 
 /// Generates a 24 word English mnemonic phrase
 pub fn generate_mnemonic() -> String {
-    let mnemonic = Mnemonic::random(&mut OsRng, Language::English);
+    let mnemonic = Mnemonic::random(OsRng, Language::English);
     mnemonic.phrase().to_string()
 }
