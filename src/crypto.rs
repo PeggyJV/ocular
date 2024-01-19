ee496b6f7e21ead79207b2f46c1f4b50fccdec0c
Collin Brittain <collinbrittain@gmail.com>
2024-01-19 13:25:06 -0600

    Bump dependencies necessary after bumping cosmrs dep, don't use reexport in tx.rs

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,5 +1,5 @@
 //! Cryptographic utilities
-use cosmrs::bip32::{Language, Mnemonic};
+use bip32::{Language, Mnemonic};
 use pkcs8::rand_core::OsRng;
 
 /// Generates a 24 word English mnemonic phrase
